f551dec1f0ba9e1b3ac50ddd94c7a51b3394b56c
Aaron Yoon <awesomeyooner@gmail.com>
2026-08-27 23:36:01 -0700

    Added many controls to StepperMotor

--- include/PolarFOC/devices/motors/stepper_motor.hpp
+++ include/PolarFOC/devices/motors/stepper_motor.hpp
@@ -7,11 +7,23 @@
 #include "PolarFOC/devices/drivers/l298n.hpp"
 #include "PolarFOC/devices/encoders/as5047.hpp"
 
+#include "EmbeddedLib/math/vector2d.hpp"
+
 
 class StepperMotor
 {
 
     public:
+
+        StepperMotor(double pole_pairs = 50);
+
+        void set_pole_pairs(double pole_pairs);
+
+        double get_pole_pairs();
+
+        void set_input_voltage(double input_voltage);
+
+        double get_input_voltage();
 
         void link_drivers(L298N* phase_A, L298N* phase_B);
 
@@ -19,21 +31,69 @@ class StepperMotor
 
         status_utils::StatusCode init();
 
-        void set_input_voltage(double input_voltage);
-
         void stop();
+
+        void refresh();
+
+        void move();
+
+        void set_target_voltage(double vq);
+
+        void set_target_voltages(double vd, double vq);
 
         void set_voltages(double vA, double vB);
 
         void set_percents(double A, double B);
 
+        void inverse_park(double vq);
+
+        void inverse_park(double vd, double vq);
+
+        void inverse_park(double vd, double vq, double mechanical_angle);
+
+        void inverse_park_openloop(double vq, double rads_per_sec);
+
+        void inverse_park_openloop(double vd, double vq, double rads_per_sec);
+
         void step(double voltage, double delay_ms = 20);
+
+        double get_electrical_angle();
+
+        void calibrate_angle_offset(double voltage);
+
+        double get_angle_offset(double voltage);
+
+        AS5047* get_encoder();
+
+        L298N* get_phase_A();
+
+        L298N* get_phase_B();
+
+        double m_openloop_angle = 0;
 
     private:
 
         AS5047* m_encoder = nullptr;
         L298N* m_phase_A = nullptr;
         L298N* m_phase_B = nullptr;
+
+        // The number of pole pairs the stepper has. Typical 1.8 deg steppers have 50
+        double m_pole_pairs = 50;
+
+        /* 
+        The target D-Axis voltage to apply during `inverse_park`. This is aligned with the rotor's magnetic flux,
+        used for field weakening. This is typically set to 0 to have the best torque performance
+        */        
+        int m_target_voltage_d = 0;
+
+        /*
+        The target Q-Axis voltage to apply during `inverse_park`. This is orthogonal to the rotor's magnetic field,
+        used for controlling torque
+        */
+        int m_target_voltage_q = 0;
+
+        // double m_openloop_angle = 0;
+        double m_openloop_timestamp = 0;
 
 
 }; // class StepperMotor
--- src/devices/motors/stepper_motor.cpp
+++ src/devices/motors/stepper_motor.cpp
@@ -5,6 +5,42 @@ using namespace std;
 using namespace status_utils;
 
 
+StepperMotor::StepperMotor(double pole_pairs)
+{
+    set_pole_pairs(pole_pairs);
+
+} // end of "StepperMotor(double = 50)"
+
+
+void StepperMotor::set_pole_pairs(double pole_pairs)
+{
+    m_pole_pairs = pole_pairs;
+
+} // end of "set_pole_pairs(int)"
+
+
+double StepperMotor::get_pole_pairs()
+{
+    return m_pole_pairs;
+
+} // end of "get_pole_pairs()"
+
+
+void StepperMotor::set_input_voltage(double input_voltage)
+{
+    m_phase_A->set_input_voltage(input_voltage);
+    m_phase_B->set_input_voltage(input_voltage);
+     
+} // end of "set_input_voltage(double)"
+
+
+double StepperMotor::get_input_voltage()
+{
+    return m_phase_A->get_input_voltage();
+
+} // end of "get_input_voltage()"
+
+
 void StepperMotor::link_drivers(L298N* phase_A, L298N* phase_B)
 {
     m_phase_A = phase_A;
@@ -36,14 +72,6 @@ StatusCode StepperMotor::init()
 } // end of "init()"
 
 
-void StepperMotor::set_input_voltage(double input_voltage)
-{
-    m_phase_A->set_input_voltage(input_voltage);
-    m_phase_B->set_input_voltage(input_voltage);
-     
-} // end of "set_input_voltage(double)"
-
-
 void StepperMotor::stop()
 {
     m_phase_A->stop();
@@ -51,6 +79,36 @@ void StepperMotor::stop()
 
 } // end of "stop()"
 
+
+void StepperMotor::refresh()
+{
+    m_encoder->refresh();
+
+} // end of "refresh()";
+
+
+void StepperMotor::move()
+{
+    inverse_park(
+        m_target_voltage_d,
+        m_target_voltage_q
+    );
+
+} // end of "move()"
+
+
+void StepperMotor::set_target_voltage(double vq)
+{
+    m_target_voltage_q = vq;
+
+} // end of "set_target_voltage(double)"
+
+
+void StepperMotor::set_target_voltages(double vd, double vq)
+{
+    m_target_voltage_d = vd;
+    m_target_voltage_q = vq;
+}
 
 void StepperMotor::set_voltages(double vA, double vB)
 {
@@ -66,6 +124,62 @@ void StepperMotor::set_percents(double A, double B)
     m_phase_B->set_percent(B);
  
 } // end of "set_percents(double, double)"
+
+
+void StepperMotor::inverse_park(double vq)
+{
+    inverse_park(0, vq);
+
+} // end of "inverse_park(double)"
+
+
+void StepperMotor::inverse_park(double vd, double vq)
+{
+    double el_angle = get_electrical_angle();
+
+    Vector2d phase_voltages = Vector2d({vd, vq}).rotate(el_angle);
+
+    double vA = phase_voltages.at(0);
+    double vB = phase_voltages.at(1);
+
+    set_voltages(vA, vB);
+
+} // end of "inverse_park(double, double)"
+
+
+void StepperMotor::inverse_park(double vd, double vq, double mechanical_angle)
+{
+    double el_angle = mechanical_angle * m_pole_pairs;
+
+    Vector2d phase_voltages = Vector2d({vd, vq}).rotate(el_angle);
+
+    double vA = phase_voltages.at(0);
+    double vB = phase_voltages.at(1);
+
+    set_voltages(vA, vB);
+
+} // end of "inverse_park(double, double, double)"
+
+
+void StepperMotor::inverse_park_openloop(double vq, double rads_per_sec)
+{
+    inverse_park_openloop(0, vq, rads_per_sec);
+}
+
+
+void StepperMotor::inverse_park_openloop(double vd, double vq, double rads_per_sec)
+{
+    double now = System::get_seconds(true);
+    double dt = now - m_openloop_timestamp;
+
+    m_openloop_angle += rads_per_sec * dt;
+
+    double mechanical_angle = m_openloop_angle;
+
+    inverse_park(vd, vq, mechanical_angle);
+
+    m_openloop_timestamp = now;
+}
 
 
 void StepperMotor::step(double voltage, double delay_ms)
@@ -109,4 +223,62 @@ void StepperMotor::step(double voltage, double delay_ms)
     m_phase_A->set_voltage(voltage);
     m_phase_B->set_voltage(-voltage);
     HAL_Delay(delay_ms);
-}
+
+} // end of "step(double, double = 20)"
+
+
+double StepperMotor::get_electrical_angle()
+{
+    return m_encoder->get_angle() * m_pole_pairs;
+
+} // end of "get_electrical_angle()"
+
+
+void StepperMotor::calibrate_angle_offset(double voltage)
+{
+    m_encoder->set_offset(
+        get_angle_offset(voltage)
+    );
+
+} // end of "calibrate_angle_offset(double)"
+
+
+double StepperMotor::get_angle_offset(double voltage)
+{
+    HAL_Delay(500);
+
+    m_phase_A->set_voltage(voltage);
+    m_phase_B->set_voltage(0);
+
+    HAL_Delay(1000);
+
+    m_encoder->refresh();
+
+    HAL_Delay(500);
+
+    stop();
+
+    return m_encoder->get_angle();
+
+} // end of "get_angle_offset(double)"
+
+
+AS5047* StepperMotor::get_encoder()
+{
+    return m_encoder;
+
+} // end of "get_encoder()"
+
+
+L298N* StepperMotor::get_phase_A()
+{
+    return m_phase_A;
+
+} // end of "get_phase_A()"
+
+
+L298N* StepperMotor::get_phase_B()
+{
+    return m_phase_B;
+
+} // end of "get_phase_B()"
